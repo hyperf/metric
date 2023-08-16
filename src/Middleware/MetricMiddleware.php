64d80349ad433f4493b7c64b322651e916a451ec
Leo Cavalcante <lc@leocavalcante.com>
2023-08-16 11:25:42 -0300

    Avoids high cardinality on metrics (#2)
    * refactor: Avoid high cardinality
    
    * refactor: Object not being used
    
    * refactor: Restore exception class labeling

--- src/Middleware/MetricMiddleware.php
+++ src/Middleware/MetricMiddleware.php
@@ -71,9 +71,6 @@ class MetricMiddleware implements MiddlewareInterface
             'request_path' => $this->getPath($request),
             'request_method' => $request->getMethod(),
             'class' => $exception::class,
-            'message' => $exception->getMessage(),
-            'code' => (string) $exception->getCode(),
-            'line' => (string) $exception->getLine(),
         ];
 
         Metric::count('exception_count', 1, $labels);
